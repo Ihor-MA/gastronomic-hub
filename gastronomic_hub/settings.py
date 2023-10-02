b84ec0287f6ddc3bbcc6234fa07a2d0df2d6b260
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-10-02 20:58:35 +0300

    Created model Dish, made migrations and migrated

--- gastronomic_hub/settings.py
+++ gastronomic_hub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "kitchen",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 AUTH_USER_MODEL = "kitchen.Cook"
+
--- kitchen/models.py
+++ kitchen/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from gastronomic_hub import settings
+
 
 class DishType(models.Model):
     name = models.CharField(max_length=255)
@@ -20,3 +22,17 @@ class Cook(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
