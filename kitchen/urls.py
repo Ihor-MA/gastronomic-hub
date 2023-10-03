ba8b9d8836964a7e04d7016b4b224b365784df0c
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-10-03 17:16:20 +0300

    created dish_type_list.html and CBW for it, added path in urls.py and added link in sidebar

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from kitchen.views import homepage, DishListView
+from kitchen.views import homepage, DishListView, DishTypeListView
 
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
 ]
 
 app_name = "kitchen"
