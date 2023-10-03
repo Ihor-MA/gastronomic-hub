c99851f4db0d4a38e7f9d56591b15286c063749e
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-10-03 08:00:44 +0300

    Сustomized the admin panel in the admin.py file for easier use

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-# Register your models here.
+from kitchen.models import Dish, DishType, Cook
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "dish_type", "price"]
+    search_fields = ("name",)
+    list_filter = ("dish_type", )
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = ["username", "first_name", "last_name", "years_of_experience", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("years_of_experience",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "years_of_experience",
+                )
+            }
+        ),
+    )
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ("name", )
+
