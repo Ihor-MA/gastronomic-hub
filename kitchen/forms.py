2a4601f6e33c34f761785878a25b4812600ea1c6
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-10-07 20:31:59 +0300

    Added search form for dish type

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -47,3 +47,16 @@ class DishSearchForm(forms.Form):
             }
         )
     )
+
+
+class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
--- kitchen/views.py
+++ kitchen/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import DishForm, CookCreationForm, CookUpdateForm, DishSearchForm
+from .forms import DishForm, CookCreationForm, CookUpdateForm, DishSearchForm, DishTypeSearchForm
 from .models import Dish, DishType, Cook
 
 
@@ -26,7 +26,7 @@ def homepage(request):
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     queryset = Dish.objects.select_related("dish_type")
-    paginate_by = 5
+    paginate_by = 6
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DishListView, self).get_context_data(**kwargs)
@@ -47,7 +47,22 @@ class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     template_name = "kitchen/dish_type_list.html"
     context_object_name = "dish_type_list"
+    queryset = DishType.objects.all()
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DishTypeListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = DishTypeSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        form = DishTypeSearchForm(self.request.GET)
+        if form.is_valid():
+            return self.queryset.filter(name__icontains=form.cleaned_data["name"])
+        return self.queryset
 
 
 class CookListView(LoginRequiredMixin, generic.ListView):
@@ -90,7 +105,7 @@ class CookCreateView(LoginRequiredMixin, generic.CreateView):
 
 class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Dish
-    fields = "__all__"
+    form_class = DishForm
     success_url = reverse_lazy("kitchen:dish-list")
 
 
